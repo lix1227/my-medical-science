a5f7bf616b5bc3243c485a7a6a2e4d49583d160c
李晓 <lix1445@fingard.com>
2022-04-15 09:31:37 +0800

    node服务端添加了与python通信方法
    (cherry picked from commit 1e2f47c2b2b418f40faddca6d224906a8b25a8d9)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,13 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const sp = require('../controller/sendToPython')
 
-router.get('/first', function (req, res, next) {
+router.get('/getPythonResult', function (req, res, next) {
+  const param = req.query.inputText
+  console.log(param)
+  sp.sendToPython([param], (res) => {
+    console.log('python的print传递过来的数据', res)
+  })
   res.send({ name: 'aaa', pwd: '123' })
 })
 module.exports = router
